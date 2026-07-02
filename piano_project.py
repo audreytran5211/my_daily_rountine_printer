ef63920db57dca1f451a9c246400d804b6c38862
audreytran5211 <m5zf8s74t7@privaterelay.appleid.com>
2026-07-02 16:38:49 -0400

    inital commit

---
+++ piano_project.py
@@ -0,0 +1,13 @@
+john = "March 14"
+michael = "July 22"
+emily = "January 9"
+sarah = "October 31"
+daniel = "May 18"
+
+
+print("Birthdays of Raj's friends:")
+print("John:", john)
+print("Michael:", michael)
+print("Emily:", emily)
+print("Sarah:", sarah)
+print("Daniel:", daniel)
